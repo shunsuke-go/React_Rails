ecc25ae0bde6b977ef9330a2a2e8fdc631b76efb
gon tana <gontana1220@gmail.com>
2021-03-07 22:42:21 +0900

    cors設定完了

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,13 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { fetchRestaurants } from '../apis/restaurant';
 
 export const Restaurants = () => {
+  useEffect(() =>  {
+      fetchRestaurants()
+      .then((data) => console.log(data)
+    )
+  },[])
+
   return (
     <Fragment>
       レストラン一覧
